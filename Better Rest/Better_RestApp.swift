b536c09b6f3705d592e2e7bf279a3748da27efa1
Balaji Srinivas <balaji21srinivas@gmail.com>
2024-02-13 20:53:33 +0530

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Balaji Srinivas on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
